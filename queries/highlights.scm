a5478a0074e803eec2f354ee4c2ed53761ba56ac
Mike Keen <mwk@mikekeen.com>
2026-04-08 01:42:22 -0400

    Refactor highlights.scm for improved syntax matching
    Updated regex patterns for global variables and simplified keyword, constant, and punctuation definitions.

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,4 +1,4 @@
-;; --- Identifiers & Variables ---
+;; 1. Variables & Identifiers
 (identifier) @variable
 
 ((identifier) @function.method
@@ -9,68 +9,35 @@
   (instance_variable)
 ] @property
 
-;; FIXED: Global variables with proper nesting and simplified regex escaping
+;; Fix for the "Error at 32": Simplified Global Variable Match
 ((global_variable) @variable.builtin
- (#match? @variable.builtin "^\\$(-[ailp]|DEBUG|FILENAME|LOAD_PATH|LOADED_FEATURES|VERBOSE|stderr|stdin|stdout|[!$&'*+,./@\\_`~])$"))
-
-(global_variable) @variable.other
+ (#match? @variable.builtin "^\\$"))
 
 [
   (self)
   (super)
 ] @variable.builtin
 
-;; --- Keywords ---
+;; 2. Keywords
 [
-  "alias"
-  "and"
-  "begin"
-  "break"
-  "case"
-  "class"
-  "def"
-  "do"
-  "else"
-  "elsif"
-  "end"
-  "ensure"
-  "for"
-  "if"
-  "in"
-  "module"
-  "next"
-  "or"
-  "rescue"
-  "retry"
-  "return"
-  "then"
-  "unless"
-  "until"
-  "when"
-  "while"
-  "yield"
+  "alias" "and" "begin" "break" "case" "class" "def" "do" "else" "elsif" 
+  "end" "ensure" "for" "if" "in" "module" "next" "or" "rescue" "retry" 
+  "return" "then" "unless" "until" "when" "while" "yield"
 ] @keyword
 
 ((identifier) @keyword
  (#match? @keyword "^(private|protected|public)$"))
 
-;; --- Constants ---
+;; 3. Constants
 (constant) @constructor
 
 ((identifier) @constant.builtin
  (#match? @constant.builtin "^__(FILE|LINE|ENCODING)__$"))
 
-(file) @constant.builtin
-(line) @constant.builtin
-(encoding) @constant.builtin
-
-(hash_splat_nil
-  "**" @operator) @constant.builtin
-
 ((constant) @constant
  (#match? @constant "^[A-Z\\d_]+$"))
 
-;; --- Functions & Methods ---
+;; 4. Functions & Methods
 "defined?" @function.method.builtin
 
 (call
@@ -79,55 +46,31 @@
 ((identifier) @function.method.builtin
  (#eq? @function.method.builtin "require"))
 
-(alias (identifier) @function.method)
-(setter (identifier) @function.method)
 (method name: [(identifier) (constant)] @function.method)
 (singleton_method name: [(identifier) (constant)] @function.method)
 
-;; --- Parameters ---
+;; 5. Parameters
 [
   (block_parameter (identifier))
-  (block_parameters (identifier))
-  (destructured_parameter (identifier))
-  (hash_splat_parameter (identifier))
-  (lambda_parameters (identifier))
   (method_parameters (identifier))
-  (splat_parameter (identifier))
   (keyword_parameter name: (identifier))
   (optional_parameter name: (identifier))
 ] @variable.parameter
 
-;; --- Literals ---
+;; 6. Literals (Fix for "Error at 65")
 [
   (string)
   (bare_string)
-  (subshell)
   (heredoc_body)
   (heredoc_beginning)
 ] @string
 
-;; FIXED: Properly nested symbol and character predicates
-((simple_symbol) @string.special.symbol
- (#match? @string.special.symbol "^:"))
-
-((character) @string.special.symbol
- (#match? @string.special.symbol "^\\?"))
-
-[
-  (delimited_symbol)
-  (hash_key_symbol)
-  (bare_symbol)
-] @string.special.symbol
-
+(simple_symbol) @string.special.symbol
 (regex) @string.special.regex
-(escape_sequence) @escape
 
 [
   (integer)
   (float)
-] @number
-
-[
   (nil)
   (true)
   (false)
@@ -139,42 +82,7 @@
 
 (comment) @comment
 
-;; --- Operators & Punctuation ---
-[
-  "="
-  "=>"
-  "->"
-  "!"
-  "?"
-  "+"
-  "-"
-  "*"
-  "/"
-  "%"
-  "**"
-  "=="
-  "!="
-  ">"
-  "<"
-  ">="
-  "<="
-  "&&"
-  "||"
-] @operator
-
-[
-  ","
-  ";"
-  "."
-] @punctuation.delimiter
-
-[
-  "("
-  ")"
-  "["
-  "]"
-  "{"
-  "}"
-  "%w("
-  "%i("
-] @punctuation.bracket
+;; 7. Operators & Punctuation
+["=" "=>" "->" "!" "?" "+" "-" "*" "/" "==" "!=" ">" "<" "&&" "||"] @operator
+["," ";" "."] @punctuation.delimiter
+["(" ")" "[" "]" "{" "}" ] @punctuation.bracket
